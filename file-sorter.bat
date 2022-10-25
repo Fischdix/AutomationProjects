e5772cb1cf1420029194d791bfb8a4a61685abfc
J F <59261070+Fischdix@users.noreply.github.com>
2022-10-25 03:50:13 -0600

    Update file-sorter.bat

--- file-sorter.bat
+++ file-sorter.bat
@@ -1,11 +1,11 @@
 @echo on
 
 PushD C:\path
-echo changing directory > name.txt
+echo changing directory
 
-echo script starting >> name.txt
+echo script starting
 For %%A in (File-name-x-x-x-x.ext) do For /F "tokens=1delims=-" %%B in ("A") Do (
 	If Not exist "x%%B" MD "x%%B"
 	Move "%%A" "x%%B"
 	)
-echo files sorted >> name.txt
+echo files sorted
